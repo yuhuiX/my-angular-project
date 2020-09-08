88d1ff1fa7dfda3b0d1d2d03843f50b0677f021c
Yuhui Xiong <yuhui.xiong.2018@gmail.com>
2020-09-08 21:53:37 +0200

    support creating initial return request from UI

--- fe/src/app/app.module.ts
+++ fe/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReturnRequestFormComponent } from './return-request-form/return-request-form.component';
 
@@ -14,6 +15,7 @@ import { ReturnRequestFormComponent } from './return-request-form/return-request
     AppRoutingModule,
     FormsModule,
     BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- fe/src/app/return-request-form/return-request-form.component.ts
+++ fe/src/app/return-request-form/return-request-form.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ReturnRequestData } from '../return-request-http/return-request-http.interfaces';
+import { ReturnRequestHttpService } from '../return-request-http/return-request-http.service';
+
 @Component({
   selector: 'app-return-request-form',
   templateUrl: './return-request-form.component.html',
-  styleUrls: ['./return-request-form.component.css']
+  styleUrls: ['./return-request-form.component.css'],
+  providers: [ReturnRequestHttpService],
 })
 export class ReturnRequestFormComponent implements OnInit {
   public returnRequestData: any = {};
 
-  constructor() { }
+  constructor(
+    private returnRequestHttpService: ReturnRequestHttpService,
+  ) {}
 
   ngOnInit(): void {
   }
 
   onSubmit(): void {
-    console.log(this.returnRequestData);
+    this.returnRequestHttpService
+      .createReturnRequest(this.returnRequestData as ReturnRequestData)
+      .subscribe((data) => {
+        console.log(data);
+      });
   }
-
 }
