d7c87d5044c69cf4edaa2ceefab4fef5af8bf94f
Yuhui Xiong <yuhui.xiong.2018@gmail.com>
2020-09-22 22:07:41 +0200

    use local guard on skus endpoint(s)

--- be/src/skus/skus.controller.ts
+++ be/src/skus/skus.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from '../auth/local-auth.guard';
 import { Sku } from './skus.interfaces';
 import { SkusService } from './skus.service';
 
+@UseGuards(LocalAuthGuard)
 @Controller('skus')
 export class SkusController {
   constructor(private readonly skusService: SkusService) {}
--- be/src/skus/skus.module.ts
+++ be/src/skus/skus.module.ts
@@ -1,9 +1,11 @@
+import { AuthService } from '../auth/auth.service';
 import { Module } from '@nestjs/common';
 import { SkusController } from './skus.controller';
 import { SkusService } from './skus.service';
+import { UsersService } from '../users/users.service';
 
 @Module({
   controllers: [SkusController],
-  providers: [SkusService],
+  providers: [AuthService, SkusService, UsersService],
 })
 export class SkusModule {}
