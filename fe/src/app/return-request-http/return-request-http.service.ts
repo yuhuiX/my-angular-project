f8a15d3c363f3115ddede6dc49c56f9afb5f4b04
Yuhui Xiong <yuhui.xiong.2018@gmail.com>
2020-09-28 22:13:54 +0200

    implement request for device finding

--- fe/src/app/return-request-http/return-request-http.service.ts
+++ fe/src/app/return-request-http/return-request-http.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -7,15 +7,39 @@ import { ReturnRequestData } from './return-request-http.interfaces';
 @Injectable()
 export class ReturnRequestHttpService {
   private ReturnRequestHttpBaseUrl = 'http://localhost:3000';
+  private devicesEndpoint = this.ReturnRequestHttpBaseUrl + '/devices';
   private returnRequestEndpoint =
     this.ReturnRequestHttpBaseUrl + '/returnRequests';
 
   constructor(private httpClient: HttpClient) {}
 
-  createReturnRequest(returnRequestData: ReturnRequestData): Observable<any> {
+  public createReturnRequest(
+    returnRequestData: ReturnRequestData,
+  ): Observable<any> {
     return this.post({
       endpoint: this.returnRequestEndpoint,
       requestBody: returnRequestData,
+    });
+  }
+
+  public findDeviceBySerialNumber(deviceSerialNumber: string): Observable<any> {
+    return this.get({
+      endpoint: this.devicesEndpoint,
+      params: new HttpParams().set('deviceSerialNumber', deviceSerialNumber),
+    });
+  }
+
+  private get({
+    endpoint,
+    params,
+  }: {
+    endpoint: string;
+    params: HttpParams;
+  }): Observable<any> {
+    return this.request({
+      endpoint,
+      method: 'get',
+      params,
     });
   }
 
@@ -36,16 +60,21 @@ export class ReturnRequestHttpService {
   private request({
     endpoint,
     method,
+    params,
     requestBody,
   }: {
     endpoint: string;
-    method: 'post';
-    requestBody: any;
+    method: 'get' | 'post';
+    params?: HttpParams;
+    requestBody?: any;
   }): Observable<any> {
     const headers: HttpHeaders = new HttpHeaders()
       .append('Content-Type', 'application/json')
       .append('Authorization', 'Basic ' + btoa('kid:secret'));
 
+    if (method === 'get') {
+      return this.httpClient[method](endpoint, { headers, params });
+    }
     if (method === 'post') {
       return this.httpClient[method](endpoint, requestBody, {
         headers,
